938f4e185153af167250eacd3eece97b0b402cc0
Valentina <maggio.vale@gmail.com>
2022-06-16 17:18:29 +0100

    Implement delete tracks from playlist
    Co-authored-by: Nicola Cortese <NicolaCortese@users.noreply.github.com>
    Co-authored-by: Samir Malik  <smm3000@users.noreply.github.com>

--- src/test/java/tech/makers/aceplay/playlist/PlaylistTest.java
+++ src/test/java/tech/makers/aceplay/playlist/PlaylistTest.java
@@ -2,6 +2,9 @@ package tech.makers.aceplay.playlist;
 
 import org.junit.jupiter.api.Test;
 import tech.makers.aceplay.playlist.Playlist;
+import tech.makers.aceplay.track.Track;
+import tech.makers.aceplay.track.TrackRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.net.MalformedURLException;
 import java.util.Set;
@@ -16,6 +19,10 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 // https://www.youtube.com/watch?v=L4vkcgRnw2g&t=1099s
 class PlaylistTest {
+
+  // @Autowired
+  // private TrackRepository trackRepository;
+
   @Test
   void testConstructs() {
     Playlist subject = new Playlist("Hello, world!", false, Set.of());
@@ -41,4 +48,21 @@ class PlaylistTest {
 
     assertEquals(1, violations.size());
   }
+
+  // @Test
+  // public void returnsPlaylistTracksByLastTrackAdded() throws
+  // MalformedURLException {
+
+  // Track track1 = new Track("Title1", "Artist1", "https://example.org/");
+  // Track track2 = new Track("Title2", "Artist2", "https://example.org/");
+  // Track track3 = new Track("Title3", "Artist3", "https://example.org/");
+
+  // Playlist subject = new Playlist("Hello, world!", false, Set.of(track1,
+  // track2, track3));
+
+  // assertEquals(3, subject.getTracks().size());
+  // assertEquals(track1, subject.getTracks().iterator().next());
+  // assertEquals(track2, subject.getTracks().iterator().next());
+  // assertEquals(track3, subject.getTracks().iterator().next());
+  // }
 }
