e158e913c4ecd6ef80a7503aa90c55d1b61bf068
Valentina <maggio.vale@gmail.com>
2022-06-20 13:15:21 +0100

    Empty fields validation for Track
    Co-authored-by: Nicola Cortese <NicolaCortese@users.noreply.github.com>
    Co-authored-by: Samir Malik  <smm3000@users.noreply.github.com>

--- src/test/java/tech/makers/aceplay/track/TrackTest.java
+++ src/test/java/tech/makers/aceplay/track/TrackTest.java
@@ -2,8 +2,12 @@ package tech.makers.aceplay.track;
 
 import org.junit.jupiter.api.Test;
 import tech.makers.aceplay.track.Track;
-
+import java.util.Set;
 import java.net.MalformedURLException;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
+import javax.validation.Validation;
+import javax.validation.ValidatorFactory;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -31,5 +35,23 @@ class TrackTest {
     Track subject = new Track();
     subject.setPublicUrl("https://example.org/track.mp3");
     assertEquals("https://example.org/track.mp3", subject.getPublicUrl().toString());
+  }
+
+  @Test
+  void testEmptyTrackTitle() throws MalformedURLException {
+    Track subject = new Track("", "TestArtist", "https://example.com");
+    Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+    Set<ConstraintViolation<Track>> violations = validator.validate(subject);
+    assertEquals(1, violations.size());
+
+  }
+
+  @Test
+  void testEmptyArtist() throws MalformedURLException {
+    Track subject = new Track("TestTitle", "", "https://example.com");
+    Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+    Set<ConstraintViolation<Track>> violations = validator.validate(subject);
+    assertEquals(1, violations.size());
+
   }
 }
